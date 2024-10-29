c7bc49b81c3ec994c9bd5ecf01e61187d9a4a1ee
Alexandr Shiriaev <alexandrshiriaev7@gmail.com>
2024-10-29 23:23:54 +0300

    refactor(tests)

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,38 +1,40 @@
-import {expect, test} from '@playwright/test';
+import {expect, Page, test} from '@playwright/test';
 
 const BASE_URL = 'https://alexandrshiriaev.github.io/progress-circle/';
 
+test.beforeEach(async ({page}) => {
+    await page.goto(BASE_URL);
+});
+
 test.describe('progress circle and control inputs are visible', () => {
     test('progress circle is visible', async ({page}) => {
-        await page.goto(BASE_URL);
+        const progressCircle = getProgressCircle(page);
 
-        await expect(page.getByRole('progressbar')).toBeVisible();
+        await expect(progressCircle).toBeVisible();
     });
 
     test('value input is visible', async ({page}) => {
-        await page.goto(BASE_URL);
+        const valueInput = getProgressValueInput(page);
 
-        await expect(page.locator('#progress-value')).toBeVisible();
+        await expect(valueInput).toBeVisible();
     });
 
     test('animate toggle button is visible', async ({page}) => {
-        await page.goto(BASE_URL);
+        const {toggleLabel} = getToggleButtonByLabel(page, 'Animate');
 
-        await expect(page.getByLabel('Animate').locator('.toggle-button__label')).toBeVisible();
+        await expect(toggleLabel).toBeVisible();
     });
 
     test('hide toggle button is visible', async ({page}) => {
-        await page.goto(BASE_URL);
+        const {toggleLabel} = getToggleButtonByLabel(page, 'Hide');
 
-        await expect(page.getByLabel('Hide').locator('.toggle-button__label')).toBeVisible();
+        await expect(toggleLabel).toBeVisible();
     });
 })
 
 test.describe('inputs are responsive', () => {
     test('value is changing on input to value input', async ({page}) => {
-        await page.goto(BASE_URL);
-
-        const valueInput = page.locator('#progress-value');
+        const valueInput = getProgressValueInput(page);
 
         await valueInput.fill('50');
 
@@ -40,122 +42,115 @@ test.describe('inputs are responsive', () => {
     })
 
     test('value is changing on clicking to animate toggle button', async ({page}) => {
-        await page.goto(BASE_URL);
+        const {toggleLabel, toggleInput} = getToggleButtonByLabel(page, 'Animate');
 
-        const animateToggleLabel = page.getByLabel('Animate').locator('.toggle-button__label');
-        const animateToggleInput = page.getByLabel('Animate').locator('.toggle-button__input');
-
-        const initValue = await animateToggleInput.isChecked();
-        await animateToggleLabel.click();
-        const newValue = await animateToggleInput.isChecked();
+        const initValue = await toggleInput.isChecked();
+        await toggleLabel.click();
+        const newValue = await toggleInput.isChecked();
         expect(newValue).toBe(!initValue);
     })
 
     test('value is changing on clicking to hide toggle button', async ({page}) => {
-        await page.goto(BASE_URL);
+        const {toggleLabel, toggleInput} = getToggleButtonByLabel(page, 'Animate');
 
-        const hideToggleLabel = page.getByLabel('Hide').locator('.toggle-button__label');
-        const hideToggleInput = page.getByLabel('Hide').locator('.toggle-button__input');
-
-        const initValue = await hideToggleInput.isChecked();
-        await hideToggleLabel.click();
-        const newValue = await hideToggleInput.isChecked();
+        const initValue = await toggleInput.isChecked();
+        await toggleLabel.click();
+        const newValue = await toggleInput.isChecked();
         expect(newValue).toBe(!initValue);
     })
 })
 
 test.describe('cannot fill input value with invalid data', () => {
     test('value is set to 0 on entering negative number', async ({page}) => {
-        await page.goto(BASE_URL);
+        const valueInput = getProgressValueInput(page);
 
-        const progressValueInput = page.locator('#progress-value');
-
-        await progressValueInput.fill('-100');
+        await valueInput.fill('-100');
         await page.dispatchEvent('#progress-value', 'change');
 
-        await expect(progressValueInput).toHaveValue('0');
+        await expect(valueInput).toHaveValue('0');
     })
 
     test('value is set to 100 on entering number greater than 100', async ({page}) => {
-        await page.goto(BASE_URL);
+        const valueInput = getProgressValueInput(page);
 
-        const progressValueInput = page.locator('#progress-value');
-
-        await progressValueInput.fill('200');
+        await valueInput.fill('200');
         await page.dispatchEvent('#progress-value', 'change');
 
-        await expect(progressValueInput).toHaveValue('100');
+        await expect(valueInput).toHaveValue('100');
     })
 
     test('value is set to parsed number from input', async ({page}) => {
-        await page.goto(BASE_URL);
+        const valueInput = getProgressValueInput(page);
 
-        const progressValueInput = page.locator('#progress-value');
-
-        await progressValueInput.fill('023');
+        await valueInput.fill('023');
         await page.dispatchEvent('#progress-value', 'change');
 
-        await expect(progressValueInput).toHaveValue('23');
+        await expect(valueInput).toHaveValue('23');
     })
 
     test('value is set to entered number on valid input', async ({page}) => {
-        await page.goto(BASE_URL);
+        const valueInput = getProgressValueInput(page);
 
-        const progressValueInput = page.locator('#progress-value');
-
-        await progressValueInput.fill('50');
+        await valueInput.fill('50');
         await page.dispatchEvent('#progress-value', 'change');
 
-        await expect(progressValueInput).toHaveValue('50');
+        await expect(valueInput).toHaveValue('50');
     })
 })
 
 test.describe('changing inputs changes state of progress circle', () => {
     test('value of progress circle is changed when value input is changed to valid value', async ({page}) => {
-        await page.goto(BASE_URL);
+        const progressCircle = getProgressCircle(page);
 
-        const progressCircle = page.locator('.progress-circle');
+        const valueInput = getProgressValueInput(page);
 
-        const progressValueInput = page.locator('#progress-value');
-
-        await progressValueInput.fill('50');
+        await valueInput.fill('50');
         await page.dispatchEvent('#progress-value', 'change');
         await expect(progressCircle).toHaveCSS('--progress-value', '180');
     })
 
     test('progress circle is animated when animate toggle is checked', async ({page}) => {
-        await page.goto(BASE_URL);
+        const progressCircle = getProgressCircle(page);
 
-        const progressCircle = page.locator('.progress-circle');
+        const {toggleLabel, toggleInput} = getToggleButtonByLabel(page, 'Animate');
 
-        const animateToggleLabel = page.getByLabel('Animate').locator('.toggle-button__label');
-        const animateToggleInput = page.getByLabel('Animate').locator('.toggle-button__input');
-
-        let isAnimated = await animateToggleInput.isChecked();
+        let isAnimated = await toggleInput.isChecked();
 
         while (!isAnimated) {
-            await animateToggleLabel.click();
-            isAnimated = await animateToggleInput.isChecked();
+            await toggleLabel.click();
+            isAnimated = await toggleInput.isChecked();
         }
 
         await expect(progressCircle).toHaveClass(/animated/);
     })
 
     test('progress circle is hidden when hide toggle is checked', async ({page}) => {
-        await page.goto(BASE_URL);
+        const progressCircle = getProgressCircle(page);
 
-        const progressCircle = page.locator('.progress-circle');
+        const {toggleLabel, toggleInput} = getToggleButtonByLabel(page, 'Hide');
 
-        const hideToggleLabel = page.getByLabel('Hide').locator('.toggle-button__label');
-        const hideToggleInput = page.getByLabel('Hide').locator('.toggle-button__input');
-
-        let isHidden = await hideToggleInput.isChecked();
+        let isHidden = await toggleInput.isChecked();
 
         while (!isHidden) {
-            await hideToggleLabel.click();
-            isHidden = await hideToggleInput.isChecked();
+            await toggleLabel.click();
+            isHidden = await toggleInput.isChecked();
         }
 
         await expect(progressCircle).toHaveClass(/hidden/);
     })
 })
+
+function getProgressCircle(page: Page) {
+    return page.getByRole('progressbar');
+}
+
+function getProgressValueInput(page: Page) {
+    return page.locator('#progress-value');
+}
+
+function getToggleButtonByLabel(page: Page, label: string) {
+    const toggleLabel = page.getByLabel(label).locator('.toggle-button__label');
+    const toggleInput = page.getByLabel(label).locator('.toggle-button__input');
+
+    return {toggleLabel, toggleInput};
+}
